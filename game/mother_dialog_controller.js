6e3ca0b81c9c2b36633ee9c9cd1d76ddce59a7c5
Elshad <shirinov.elshad@gmail.com>
2013-11-07 22:41:29 +0100

    Simplified the dialog templates

--- game/mother_dialog_controller.js
+++ game/mother_dialog_controller.js
@@ -1,64 +1,71 @@
 'use strict';
 
-var otherThemeDialogTemplate = [
-	{type: 'options', options: [
+var otherTopicDialogTemplate = [
+	{options: [
 		{text: 'Nichts', consequence: [
-			{type: 'action', action: function(scene, world) { scene.onExit(); } }
+			{left: 'Nein, Mama. War schön mit dir zu reden.', wait: 1},
+			{right: 'OK. Dann meld dich mal wieder. Bis dann.', wait: 4},
+			{right: '*tuut*', wait: 1},
+			{right: '*tuut*', wait: 1},
+			{right: '*tuut*', wait: 1},
+			{right: '*tuut*', wait: 1},
+			{right: '*tuut*', wait: 1},
+			function(scene, world) { scene.onExit(); }
 		]}
 	]}
 ];
 
 var workDialogTemplate = [
-	{type: 'options', options: [
+	{options: [
 		{text: 'Sehr gut', consequence: [
-			{type: 'line', speaker: 'left', text: 'Es ist sehr schön. Die Kollegen sind sehr nett und der Chef auch.', wait: 1},
-			{type: 'line', speaker: 'right', text: 'Na siehst du.', wait: 1},
-			{type: 'line', speaker: 'right', text: 'Ich war mir sicher, dass du alles gut schaffst.', wait: 1},
-			{type: 'action', action: function(scene, world) { world.player.fun += 10; } },
-			{type: 'line', speaker: 'left', text: 'Und gibt\'s sonst noch etwas?', wait: 1},
-			{type: 'template', template: otherThemeDialogTemplate}
+			{left: 'Es ist sehr schön. Die Kollegen sind sehr nett und der Chef auch.', wait: 1},
+			{right: 'Na siehst du.', wait: 1},
+			{right: 'Ich war mir sicher, dass du alles gut schaffst.', wait: 1},
+			function(scene, world) { world.player.fun += 10; },
+			{right: 'Und gibt\'s sonst noch etwas?', wait: 1},
+			otherTopicDialogTemplate
 		]},
 		{text: 'Nicht so gut', consequence: []}
 	]}
 ];
 
 var goodMoodDialogTemplate = [
-	{type: 'line', speaker: 'left', text: 'Gerade läuft einfach alles sehr gut.', wait: 1},
-	{type: 'line', speaker: 'right', text: 'Das freut mich aber.', wait: 1},
-	{type: 'line', speaker: 'right', text: 'Wie läuft denn die neue Arbeit?', wait: 1},
-	{type: 'template', template: workDialogTemplate}
+	{left: 'Gerade läuft einfach alles sehr gut.', wait: 1},
+	{right: 'Das freut mich aber.', wait: 1},
+	{right: 'Wie läuft denn die neue Arbeit?', wait: 1},
+	workDialogTemplate
 ];
 
 var motherDialogTemplate = [
-	{type: 'line', speaker: 'left', text: '*klingel*', wait: 1},
-	{type: 'line', speaker: 'left', text: '*klingel*', wait: 1},
-	{type: 'line', speaker: 'left', text: '*klingel*', wait: 1},
-	{type: 'line', speaker: 'right', text: 'Hallo.', wait: 0.5},
-	{type: 'options', options: [
+	{right: '*klingel*', wait: 1},
+	{right: '*klingel*', wait: 1},
+	{right: '*klingel*', wait: 1},
+	{right: 'Hallo.', wait: 0.5},
+	{options: [
 		{text: 'Grüße sie', consequence: [
-			{type: 'line', speaker: 'left', text: 'Hallo, Mama.', wait: 1},
-			{type: 'line', speaker: 'right', text: 'Ah, hallo mein Junge. Wie geht\'s dir?', wait: 0.5},
-			{type: 'options', options: [
+			{left: 'Hallo, Mama.', wait: 1},
+			{right: 'Ah, hallo mein Junge. Wie geht\'s dir?', wait: 0.5},
+			{options: [
 				{text: 'Alles ok', consequence: [
-					{type: 'line', speaker: 'left', text: 'Es ist alles in Ordnung. Wie geht\'s dir, denn?', wait: 1},
-					{type: 'line', speaker: 'right', text: 'Ach alles ist beim alten, wie läuft deine neue Arbeit?', wait: 1},
+					{left: 'Es ist alles in Ordnung. Wie geht\'s dir, denn?', wait: 1},
+					{right: 'Ach alles ist beim alten, wie läuft deine neue Arbeit?', wait: 1},
 				]},
 				{text: 'Alles super!', consequence: goodMoodDialogTemplate},
 				{text: 'So la-la', consequence: []}
 			]}
 		]},
 		{text: 'Grüße sie fröhlich', consequence: [
-			{type: 'line', speaker: 'left', text: 'Hallo, Mama!!', wait: 1},
-			{type: 'line', speaker: 'right', text: 'Ach, hallo mein Junge. So gut gelaunt?', wait: 0.5},
-			{type: 'options', options: [
+			{left: 'Hallo, Mama!!', wait: 1},
+			{right: 'Ach, hallo mein Junge. So gut gelaunt?', wait: 0.5},
+			{options: [
 				{text: 'Einfach gute Laune', consequence: goodMoodDialogTemplate},
 				{text: 'Neue Arbeit läuft toll', consequence: []}
 			]}
 		]},
 		{text: 'Schluchze traurig', consequence: [
-			{type: 'line', speaker: 'left', text: 'Hallo *schluchz*, Mama.', wait: 1},
-			{type: 'line', speaker: 'right', text: 'Mein Junge, was ist denn passiert?', wait: 0.5},
-			{type: 'options', options: [
+			{left: 'Hallo *schluchz*, Mama.', wait: 1},
+			{right: 'Mein Junge, was ist denn passiert?', wait: 0.5},
+			{options: [
 				{text: '[Lügen] Nichts...', consequence: []},
 				{text: 'Neue Arbeit läuft schlecht', consequence: []}
 			]}
@@ -67,38 +74,35 @@ var motherDialogTemplate = [
 ];
 
 function loadDialogElement(element, actions, scene, world) {
-	switch (element.type) {
-		case 'line':
+	if (element.left || element.right) {
+		if (element.left) {
 			actions.push(new InstantAction(function() { 
-				if (element.speaker == 'left') {
-					scene.addMyLine(element.text);
-				} else if (element.speaker == 'right') {
-					scene.addTheirLine(element.text);
-				}
+				scene.addMyLine(element.left);
 			}));
-			if (element.wait) {
-				actions.push(new WaitAction(element.wait));
-			}
-			break;
-		case 'options':
-			actions.push(new InstantAction(function() {
-				element.options.forEach(function(option) {
-					scene.addOption(option.text, function() {
-						scene.clearOptions();
-						loadDialog(option.consequence, scene, world);
-					});
+		} else if (element.right) {
+			actions.push(new InstantAction(function() { 
+				scene.addTheirLine(element.right);
+			}));
+		}
+		if (element.wait) {
+			actions.push(new WaitAction(element.wait));
+		}
+	} else if (element.options) {
+		actions.push(new InstantAction(function() {
+			element.options.forEach(function(option) {
+				scene.addOption(option.text, function() {
+					scene.clearOptions();
+					loadDialog(option.consequence, scene, world);
 				});
-			}));
-			break;
-		case 'template':
-			loadDialogTemplate(element.template, actions, scene, world);
-			break;
-		case 'action':
-			actions.push(new InstantAction(function() {
-				element.action(scene, world);
-			}));
-			break;
-	}	
+			});
+		}));		
+	} else if (Array.isArray(element)) {
+		loadDialogTemplate(element, actions, scene, world);
+	} else if ((typeof element) == 'function') {
+		actions.push(new InstantAction(function() {
+			element(scene, world);
+		}));
+	}
 }
 
 function loadDialogTemplate(template, actions, scene, world) {
