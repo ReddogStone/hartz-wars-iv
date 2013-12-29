475cff49850825ba43599e9309f7f718f132b787
Elshad <shirinov.elshad@gmail.com>
2013-12-29 12:34:40 +0100

    Camera rotation around target works

--- 3dengine/camera.js
+++ 3dengine/camera.js
@@ -12,6 +12,7 @@ function Camera(fov, aspect, nearPlane, farPlane) {
 	this._far = farPlane;
 	
 	this.target = null;
+	this.targetTransformable = null;
 	
 	this._projection = null;
 	this._createProjection();
@@ -20,22 +21,43 @@ Camera.extends(Object, {
 	_createProjection: function() {
 		this._projection = new Vecmath.Matrix4().perspective(this._fov, this._aspect, this._near, this._far);
 	},
-	getView: function(transformable) {
+	_getTargetPos: function() {
 		if (this.target) {
-			var target = this.target;
-			var targetPos = target;
-			if (!(target instanceof Vecmath.Vector3)) {
-				targetPos = target.pos;
-			}
-			return new Vecmath.Matrix4().lookAt(transformable.pos, targetPos, transformable.up);
-		} else {
-			return new Vecmath.Matrix4().fromRotationTranslation(transformable.rot, transformable.pos).invert();
+			return this.target;
+		} else if (this.targetTransformable) {
+			return this.targetTransformable.pos;
 		}
+		return null;
+	},
+	getView: function(transformable) {
+		var targetPos = this._getTargetPos();
+		if (targetPos) {
+			return new Vecmath.Matrix4().lookAt(transformable.pos, targetPos, transformable.up);
+		}
+		return new Vecmath.Matrix4().fromRotationTranslation(transformable.rot, transformable.pos).invert();
 	},
 	getProjection: function() {
 		return this._projection;
 	},
-	rotateAroundTarget: function(transformable) {
-		
+	rotateAroundTargetVert: function(transformable, angle) {
+		var targetPos = this._getTargetPos().clone();
+		if (targetPos) {
+			var targetToPos = transformable.pos.clone().sub(targetPos);
+			var up = transformable.up;
+			var right = up.clone().cross(targetToPos).normalize();
+			var rotation = new Vecmath.Quaternion().setAxisAngle(right, angle);
+			transformable.pos = targetPos.add(targetToPos.transformQuat(rotation));
+		}
+	},
+	rotateAroundTargetHoriz: function(transformable, angle) {
+		var targetPos = this._getTargetPos().clone();
+		if (targetPos) {
+			var targetToPos = transformable.pos.clone().sub(targetPos);
+			var up = transformable.up;
+			var right = up.clone().cross(targetToPos).normalize();
+			up = targetToPos.clone().cross(right).normalize();
+			var rotation = new Vecmath.Quaternion().setAxisAngle(up, angle);
+			transformable.pos = targetPos.add(targetToPos.transformQuat(rotation));
+		}
 	}
 });
